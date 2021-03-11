e30139dd528a3561579c6f99f2806349fccf6ba8
Divyesh Rana <iit2019063@iiita.ac.in>
2021-03-11 19:09:40 +0530

    Add files via upload

---
+++ Assignment 4/naive.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+
+
+	int n;
+	// cin >> n;
+	scanf("%d", &n);
+	int A[n][n], B[n][n], C[n][n];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)scanf("%d", &A[i][j]);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)scanf("%d", &B[i][j]);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			C[i][j] = 0;
+			for (int k = 0; k < n; k++) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", C[i][j]);
+		}
+		printf("\n");
+	}
+}
